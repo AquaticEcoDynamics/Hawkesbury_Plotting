d508ff6839682fb1db5d84b7f1c1707004eea80f
brendan-busch <brendan.busch@uwa.edu.au>
2022-07-01 10:05:07 +0800

    Updated variables

---
+++ polygon_timeseries_plotting/modeltools/add_vars.m
@@ -0,0 +1,51 @@
+clear all; close all;
+
+load hawkesbury_all.mat;
+
+[snum,sstr] = xlsread('WQ Module Variable Names.xlsx','A2:C16');
+
+aedname = sstr(:,1);
+newname = sstr(:,2);
+conv = snum(:,1);
+
+sites = fieldnames(hawkesbury_all);
+
+for i = 1:length(newname)
+    for j = 1:length(sites)
+        vars = fieldnames(hawkesbury_all.(sites{j}));
+        
+        for k = 1:length(vars)
+           sss = find(strcmpi(aedname,vars{k}) == 1);
+           if ~isempty(sss)
+               disp(newname{sss});
+               hawkesbury_all.(sites{j}).(newname{sss}) = hawkesbury_all.(sites{j}).(aedname{sss});
+               hawkesbury_all.(sites{j}).(newname{sss}).Data = hawkesbury_all.(sites{j}).(newname{sss}).Data * conv(sss);
+           end
+        end
+    end
+end
+
+[snum,sstr] = xlsread('WQ Module Variable Names.xlsx','D2:F16');
+
+aedname = sstr(:,1);
+newname = sstr(:,2);
+conv = snum(:,1);
+
+sites = fieldnames(hawkesbury_all);
+
+for i = 1:length(newname)
+    for j = 1:length(sites)
+        vars = fieldnames(hawkesbury_all.(sites{j}));
+        
+        for k = 1:length(vars)
+           sss = find(strcmpi(aedname,vars{k}) == 1);
+           if ~isempty(sss)
+               disp(newname{sss});
+               hawkesbury_all.(sites{j}).(newname{sss}) = hawkesbury_all.(sites{j}).(aedname{sss});
+               hawkesbury_all.(sites{j}).(newname{sss}).Data = hawkesbury_all.(sites{j}).(newname{sss}).Data * conv(sss);
+           end
+        end
+    end
+end
+        
+        
